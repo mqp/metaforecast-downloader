9c37416f6ece485da0291687112e33fa5736b2f1
baseratetimes <marcelvandiemen@protonmail.com>
2023-08-20 08:20:15 +1000

    Trying auto updating headline for Putin

--- Putin.js
+++ Putin.js
@@ -20,3 +20,38 @@ function getPoint(id, historyItem) {
 }
 
 fetchAll(markets, getPoint).then((data) => writeJSONOutput("Putin.json", data));
+
+// Automatically updating headlines
+
+fetchAll(markets, getPoint).then((data) => {
+  // Extract the latest probability values for each market
+  const latestProbabilities = data.map(market => {
+    const points = market.points;
+    const lastPoint = points[points.length - 1];
+    return lastPoint.y; // Assuming y represents the probability
+  });
+
+  // Calculate the min and max probabilities
+  const minProbability = Math.min(...latestProbabilities);
+  const maxProbability = Math.max(...latestProbabilities);
+
+  let headline;
+  // Check if all probabilities are within 5% of each other
+  if (maxProbability - minProbability <= 5) {
+    // Calculate the median
+    latestProbabilities.sort((a, b) => a - b);
+    const midIndex = Math.floor(latestProbabilities.length / 2);
+    const medianProbability = latestProbabilities.length % 2 === 0
+      ? (latestProbabilities[midIndex - 1] + latestProbabilities[midIndex]) / 2
+      : latestProbabilities[midIndex];
+
+    // Create the headline with the median probability
+    headline = `Putin: ${medianProbability.toFixed(2)}% likely to remain in power`;
+  } else {
+    // Create the headline with the range of probabilities
+    headline = `Putin: ${minProbability.toFixed(2)}% - ${maxProbability.toFixed(2)}% likely to remain in power`;
+  }
+
+  // Insert the headline into the HTML element with id "headline"
+  document.getElementById("headlinePutin").textContent = headline;
+});
