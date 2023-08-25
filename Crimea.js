b69f541322de97990725e89b92f9cd798cbf78af
baseratetimes <marcelvandiemen@protonmail.com>
2023-08-25 13:12:15 +1000

    Re added Poly market on land bridge

--- Crimea.js
+++ Crimea.js
@@ -4,7 +4,7 @@ const markets = [
   { name: "Crimea land bridge cut?", id: "metaculus-13531" },
   { name: "Crimea any territory retaken by Dec 15?", id: "goodjudgmentopen-2967" }, // Updated to new forecast id - Marcel
   { name: "Crimea retaken?", id: "manifold-8dD3vNDbHnPCx3movLl9" },
-  // { name: "Crimea land bridge cut by Nov?", id: "polymarket-0xda2cef9f" }, // This has fallen of Metaforecast?
+  { name: "Crimea land bridge cut by Nov?", id: "polymarket-0xda2cef9f" }, // This has fallen of Metaforecast?
   { name: "Crimea land bridge cut by Oct?", id: "insight-154445" },
   { name: "Crimea largest city retaken?", id: "metaculus-10737" }, // Added popular Metaculus market - Marcel
 ];
