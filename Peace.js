96f6dee3f79ee5c8cc6a792146d21eae5f48a1dd
baseratetimes <marcelvandiemen@protonmail.com>
2023-08-30 20:38:34 +1000

    GJO market updated

--- Peace.js
+++ Peace.js
@@ -2,14 +2,14 @@ import { fetchAll, writeJSONOutput } from './helpers.js';
 
 const markets = [
   { name: "Ceasefire?", id: "metaculus-13985" },
-  { name: "Ceasefire?", id: "polymarket-0x30afc488" }, // New market
-  { name: "War ends by Aug 19?", id: "goodjudgmentopen-2657" }, // Updated to capture correct date
+  { name: "Ceasefire?", id: "polymarket-0x30afc488" },
+  { name: "War ends?", id: "goodjudgmentopen-2951" },
   { name: "War ends?", id: "manifold-IY4cZAXStA3cvcqCDJqR" },
   { name: "War ends?", id: "insight-224920" }
 ];
 
 function getPoint(id, historyItem) {
-  const targetOption = id === 'goodjudgmentopen-2657' ? 'Between 24 May 2023 and 18 August 2023' : 'Yes';
+  const targetOption = id === 'goodjudgmentopen-2951' ? 'Before 1 January 2024' : 'Yes';
   for (const option of historyItem.options) {
     if (option.name === targetOption) {
       return { x: historyItem.fetched * 1000, y: option.probability }
