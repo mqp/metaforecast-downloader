ee39bfc95a8a1afa4a41f7f144ea639c35b6ee06
baseratetimes <marcelvandiemen@protonmail.com>
2024-01-17 14:11:08 +1100

    moved some stuff around

--- IsraelHamasIran2.js
+++ IsraelHamasIran2.js
@@ -3,9 +3,9 @@ import { fetchAll, writeJSONOutput } from './helpers.js';
 const markets = [
 //  { name: "by Oct?", id: "goodjudgmentopen-2617" },
 //  { name: "Iran nuke facilities attacked?", id: "infer-1289" },
-  { name: "90% enriched uranium in Iran by 2025?", id: "infer-1286" },
-  { name: "Iran nukes 'likely' by Jul 2024?", id: "goodjudgmentopen-2552" },
-  { name: "Iran has nukes by 2030?", id: "metaculus-5253" },
+//  { name: "90% enriched uranium in Iran by 2025?", id: "infer-1286" },
+//Moved to Nuclear  { name: "Iran nukes 'likely' by Jul 2024?", id: "goodjudgmentopen-2552" },
+//Moved to Nuclear  { name: "Iran has nukes by 2030?", id: "metaculus-5253" },
 ];
 
 function getPoint(id, historyItem) {
--- Nuclear.js
+++ Nuclear.js
@@ -3,11 +3,14 @@ import { fetchAll, writeJSONOutput } from './helpers.js';
 const markets = [
   { name: "RUS detonates nuke outside of RUS/BLR by May?", id: "goodjudgmentopen-3017" },
   { name: "RUS moves nuke into UKR boundries by May?", id: "goodjudgmentopen-3016" },
-  { name: "ISR uses nuke in combat by Oct 7?", id: "metaculus-19554" },
-  { name: "UKR starts nuke program by 2025?", id: "metaculus-17791" },
+//Broken on Poly but good for 2024  { name: "ISR uses nuke in combat by Oct 7?", id: "metaculus-19554" },
+//Broken on Poly but good for 2024  { name: "UKR starts nuke program by 2025?", id: "metaculus-17791" },
+{ name: "Iran nukes 'likely' by Jul 2024?", id: "goodjudgmentopen-2552" },
+{ name: "Iran has nukes by 2030?", id: "metaculus-5253" },
+
 //  { name: "Radiation incident at Ukr plant?", id: "metaculus-10154" },
 //  { name: "Rus nukes Ukr?", id: "metaculus-13171" },
-//  { name: "Non-test nuke used?", id: "metaculus-2797" }, // Updated to capture No
+//  { name: "Non-test nuke used?", id: "metaculus-2797" },
 //  { name: "Nuke used in combat?", id: "manifold-CBCpG7AUU4pnsbyBn0bg" },
 //  { name: "Rus nukes Ukr by Oct?", id: "goodjudgmentopen-2634" },
 ];
