c5cdea8c3fb163345f07766b10b19987ca11c880
baseratetimes <marcelvandiemen@protonmail.com>
2024-01-19 17:35:03 +1100

    hamas gaza control update

--- IsraelHamasGazaControl.js
+++ IsraelHamasGazaControl.js
@@ -5,8 +5,11 @@ const markets = [
 //  { id: "metaculus-19308" },
 //  { id: "manifold-kVw4c6CXZlCUAhY1laVR" },
 // { name: "Hamas loses power by Feb?", id: "polymarket-0xfeabd92e" },
-{ name: "Hamas loses power by Jan 15?", id: "manifold-FsaZWdy6oL6gRZJe8RGJ" },
+// { name: "Hamas loses power by Jan 15?", id: "manifold-FsaZWdy6oL6gRZJe8RGJ" },
 { name: "IDF exits Gaza by Oct 10?", id: "goodjudgmentopen-3069" },
+{ name: "UN troops enter Gaza by Apr?", id: "metaculus-19849" },
+{ name: "Hamas loses power in Gaza by Apr?", id: "manifold-JNk4oJ5uVu6nsnsXJeOH" },
+{ name: "Israel occupies Gaza in 2024?", id: "manifold-r8ll07OIvKvXOryaTKT1" },
 ];
 
 function getPoint(id, historyItem) {
